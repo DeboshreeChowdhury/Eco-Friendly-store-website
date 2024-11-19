f73ff1d89f17ca455fb8c492e1a9c0d5d4d09312
Deboshree Chowhdury <deboshreechow96@gmail.com>
2024-11-18 19:03:02 -0500

    Modified some pages

--- main.js
+++ main.js
@@ -79,3 +79,10 @@ document.querySelectorAll(".btn-success").forEach(button => {
         }
     });
 });
+const backToTop = document.getElementById("backToTop");
+window.onscroll = function () {
+    backToTop.style.display = window.scrollY > 300 ? "block" : "none";
+};
+function scrollToTop() {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+}
